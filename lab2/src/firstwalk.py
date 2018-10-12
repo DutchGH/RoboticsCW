d72e655ec5e4ef0d42efbfb199e25e15f5a20a2e
Jacob Holland [sc15j3h] <sc15j3h@comp-pc6163.leeds.ac.uk>
2018-10-12 13:07:32 +0100

    Bad collision code. TODO: REFACTOR

--- lab2/src/firstwalk.py
+++ lab2/src/firstwalk.py
@@ -2,20 +2,87 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from kobuki_msgs.msg import BumperEvent
+
+shouldMove = True
+def callback(data):
+	global shouldMove
+	rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.state)
+	if data.state :
+		shouldMove = False
+
 
 def publisher():
-	pub = rospy.Publisher('mobile_base/commands/velocity', Twist)
+	global shouldMove
+	pub = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size=10)
 	rospy.init_node('Walker', anonymous=True)
 	rate = rospy.Rate(10) #10hz
+
+	rospy.Subscriber('mobile_base/events/bumper', BumperEvent, callback)
+
 	desired_velocity = Twist()
-	desired_velocity.linear.x = 0.2 # Forward with 0.2 m/sec.
-	for i in range (30):
-		pub.publish(desired_velocity)
-		rate.sleep()
-	desired_velocity.linear.x = -0.2 # Backward with 0.2 m/sec.
-	for i in range(30):
-		pub.publish(desired_velocity)
-		rate.sleep()
+	# desired_velocity.linear.x = 0.2# Forward with 0.2 m/sec.
+	# desired_velocity.angular.z = 0.5
+	# 	pub.publish(desired_velocity)
+	# 	rate.sleep()
+
+	# for i in range (30):
+	while not rospy.is_shutdown():
+		desired_velocity.linear.x = 0.2 # Backward with 0.2 m/sec.
+		for i in range(30):
+			if (shouldMove):
+				pub.publish(desired_velocity)
+				rate.sleep()
+
+		desired_velocity.linear.x = 0
+		desired_velocity.angular.z = -1.5708
+		for i in range(10):
+			if (shouldMove):
+				pub.publish(desired_velocity)
+				rate.sleep()
+
+		desired_velocity.linear.x = 0.2
+		desired_velocity.angular.z = 0
+		for i in range(30):
+			if (shouldMove):
+				pub.publish(desired_velocity)
+				rate.sleep()
+
+		desired_velocity.linear.x = 0
+		desired_velocity.angular.z = -1.5708
+		for i in range(10):
+			if (shouldMove):
+				pub.publish(desired_velocity)
+				rate.sleep()
+
+		desired_velocity.linear.x = 0.2
+		desired_velocity.angular.z = 0
+		for i in range(30):
+			if (shouldMove):
+				pub.publish(desired_velocity)
+				rate.sleep()
+
+		desired_velocity.linear.x = 0
+		desired_velocity.angular.z = -1.5708
+		for i in range(10):
+			if (shouldMove):
+				pub.publish(desired_velocity)
+				rate.sleep()
+
+		desired_velocity.linear.x = 0.2
+		desired_velocity.angular.z = 0
+		for i in range(30):
+			if (shouldMove):
+				pub.publish(desired_velocity)
+				rate.sleep()
+
+		desired_velocity.linear.x = 0
+		desired_velocity.angular.z = -1.5708
+		for i in range(10):
+			if (shouldMove):
+				pub.publish(desired_velocity)
+				rate.sleep()
+
 
 if __name__ == "__main__":
 	try:
